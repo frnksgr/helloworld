c9cd1a364ef0671b28185af47f28a9873fa1a27e
Frank Seeger (d030837) <f.seeger@sap.com>
2018-07-30 12:12:26 +0000

    code cleanup

--- main.go
+++ main.go
@@ -11,29 +11,29 @@ import (
 
 const defaultPort = 8080
 
-func main() {
-	var (
-		port     int
-		listener net.Listener
-		err      error
-		logger   *log.Logger
-	)
+// logger writing to stdout
+var logger = log.New(os.Stdout, "APP ", log.Lshortfile)
+
+// get port from env or use defaultPort
+func getPort() int {
+	var port = defaultPort
+	var err error
 
 	if value, hasPort := os.LookupEnv("PORT"); hasPort {
 		if port, err = strconv.Atoi(value); err != nil {
-			log.Println("Cannot read PORT from environment")
-			log.Fatal(err)
+			logger.Println("Cannot convert PORT from environment")
+			logger.Printf("Using defaut port: %d\n", defaultPort)
 		}
 	} else {
-		port = defaultPort
+		logger.Println("No PORT env variable found")
+		logger.Printf("Using defaut port: %d\n", defaultPort)
 	}
+	return port
+}
 
-	listener, err = net.Listen("tcp4", ":"+strconv.Itoa(port))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	logger = log.New(os.Stdout, "APP ", log.Lshortfile)
+func main() {
+	var port = getPort()
+	var listener, _ = net.Listen("tcp4", ":"+strconv.Itoa(port))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!\n")
@@ -41,5 +41,5 @@ func main() {
 	})
 
 	logger.Printf("Starting server on port %d ...", port)
-	log.Fatal(http.Serve(listener, nil))
+	logger.Fatal(http.Serve(listener, nil))
 }
