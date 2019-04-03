93af98e3a2b5b95b38227184ff0f860e677afc21
d030837 <f.seeger@sap.com>
2019-04-03 10:42:48 +0200

    added /info handler

--- main.go
+++ main.go
@@ -40,15 +40,30 @@ func getFrom() string {
 	}
 }
 
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("%s %s %s \n", r.Method, r.URL, r.Proto)
+	fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
+	//Iterate over all header fields
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
+	//Get value for a specified token
+	//fmt.Fprintf(w, "\n\nFinding value of \"Accept\" %q", r.Header["Accept"])
+}
+
 func main() {
 	var port = getPort()
 	var listener, _ = net.Listen("tcp4", ":"+strconv.Itoa(port))
 	var from = getFrom()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logger.Printf("%s %s %s \n", r.Method, r.URL, r.Proto)
 		fmt.Fprintf(w, "Hello World!\nFrom: %s\n", from)
-		logger.Println(r.Proto, r.Host, r.Method, r.URL)
 	})
+
+	http.HandleFunc("/info", infoHandler)
 
 	logger.Printf("Starting server on port %d ...", port)
 	logger.Fatal(http.Serve(listener, nil))
